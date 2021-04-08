8bf1b3b80abc6f55530e2a8a244ab4c8652f9c8b
Olivia Lewis <ollybaby12@gmail.com>
2021-04-08 18:48:49 -0400

    prep questions endpoint

--- src/app.js
+++ src/app.js
@@ -4,6 +4,17 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV, CLIENT_ORIGIN } = require('./config')
+const logger = require('./logger')
+const questionsRouter = require('./questions/questions-router')
+
+// const questions = [
+//     {
+//         "id": "9f483f4f-d718-485a-85e4-978808a99f8e",
+//         "question": "Tell me about yourself.",
+//         "guidance": "This is often the first of many interview questions, designed to ‘warm up’ the candidate. Many candidates choose to respond with an overview of their work and employment history. Whilst this is helpful – especially if a manager hasn’t read your CV in detail, it’s important you offer new information, such as what are your hobbies outside of work. It’s also easy to fall into the trap of waffling. Other candidates prefer to focus on a key aspect of their career, building a story around it with performance highlights. It’s important to ensure your answer is aligned with the job description and advertisement, to demonstrate how you can add value to the company and role at hand.",
+//         "response": {}
+//     }
+// ]
 
 const app = express()
 
@@ -20,11 +31,21 @@ app.use(
     })
 );
 
-app.get('/api/*', (req, res) => {
-    res.json({ ok: true });
-});
+// app.use(function validateBearerToken(req, res, next) {
+//     const apiToken = process.env.API_TOKEN
+//     const authToken = req.get('Authorization')
 
-app.get('/', (req, res) => {
+//     if (!authToken || authToken.split(' ')[1] !== apiToken) {
+//         logger.error(`Unauthorized request to path: ${req.path}`);
+//         return res.status(401).json({ error: 'Unauthorized request' })
+//     }
+//     //move to next middleware
+//     next()
+// })
+
+app.use('/api/questions', questionsRouter)
+
+app.get('/', (req, res, next) => {
     res.send('Hello, world!')
 })
 
